8749520d74f6db06a501b282e8d7d57d393b6b16
non-97 <nonmonaka07@gmail.com>
2024-11-06 14:24:58 +0900

    Initial commit

---
+++ test/fsxn-resources.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as FsxnResources from '../lib/fsxn-resources-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/fsxn-resources-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new FsxnResources.FsxnResourcesStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
